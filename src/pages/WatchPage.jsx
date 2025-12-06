a5f71bf2b007bda1f039e63a41a28eaf20a6a676
kingTITIdebugger9000 <umarfarhan967@gmail.com>
2025-12-06 17:09:28 +0500

    Fix watch API URL

--- src/pages/WatchPage.jsx
+++ src/pages/WatchPage.jsx
@@ -33,7 +33,7 @@ const WatchPage = () => {
   useEffect(() => {
     const fetchDetails = async () => {
       try {
-        const res = await fetch(`<YOURHOSTED.vercel.app/api/anime/${id}`);
+        const res = await fetch(`https://titianime.vercel.app/api/episode/${id}`);
         const json = await res.json();
         if (json.success) setAnimeDetails(json.data);
       } catch (err) {
